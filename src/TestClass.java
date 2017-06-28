4e38f89ee835028d9aae3f6f7ede93ed1320909c
JoaoMSilva <joaomsfrs@gmail.com>
2017-06-28 20:51:17 +0100

    Versão João

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,9 @@ public class TestClass {
 	public static void main(String[] args) {
 
 		System.out.println("Ola"); 
+		
+		TotalAnarchy ta = new TotalAnarchy();
+		ta.showCnt();
 
 	}
 
---
+++ src/TotalAnarchy.java
@@ -0,0 +1,14 @@
+
+public class TotalAnarchy {
+	
+	int cntAnarquia;
+	
+	public TotalAnarchy() {
+		cntAnarquia = 10;
+	}
+	
+	public void showCnt() {
+		System.out.println("cntAnarquia = "+cntAnarquia);
+	}
+
+}
